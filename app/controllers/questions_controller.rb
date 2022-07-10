fc24ed8796dd351a9eeccd5010b3dac3383d4f3a
leont <leon.traykov@gmail.com>
2022-07-10 12:42:58 +0700

    second commit - Ask me - Version 1.2 - with notices

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,23 +3,24 @@ class QuestionsController < ApplicationController
   def create
     question = Question.create(question_params)
 
-    redirect_to question
+    redirect_to question, notice: "New question has been asked"
   end
 
   def index
-    # debugger
+    @question = Question.new
     @questions = Question.all
   end
 
   def update
     @question.update(question_params)
-    redirect_to @question
+
+    redirect_to @question, notice: "Question has been edited"
   end
 
   def destroy
     @question.destroy
 
-    redirect_to questions_path
+    redirect_to questions_path, notice: "Question has been deleted"
   end
 
   def show
