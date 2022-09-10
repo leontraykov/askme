a0692e7c9b1c40501ae13c6e7d3562b94c846444
leont <leon.traykov@gmail.com>
2022-09-10 16:19:14 +0700

    username adress - first try

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
 
-  resources :users, except: %i[index]
+  get 'users/:nickname', to: 'users#show', as: :user
+  resources :users, except: %i[index show]
   resource :session, only: %i[new create destroy]
-  
+
   resources :questions do
     put :hide, on: :member
     patch :hide, on: :member
